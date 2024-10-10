23e6472585e634861bbf6795531b1422f9f51116
stefano <stefanolindenbojm@gmail.com>
2024-10-10 13:34:41 +0000

    ac

--- main/main.c
+++ main/main.c
@@ -2,91 +2,134 @@
 #include <task.h>
 #include <semphr.h>
 #include <queue.h>
-
 #include "pico/stdlib.h"
 #include <stdio.h>
-
 #include "hardware/gpio.h"
 #include "hardware/i2c.h"
 #include "mpu6050.h"
-
 #include <Fusion.h>
 
 const int MPU_ADDRESS = 0x68;
 const int I2C_SDA_GPIO = 4;
 const int I2C_SCL_GPIO = 5;
+#define SAMPLE_PERIOD (0.01f)
 
-static void mpu6050_reset() {
-    // Two byte reset. First byte register, second byte data
-    // There are a load more options to set up the device in different ways that could be added here
-    uint8_t buf[] = {0x6B, 0x00};
-    i2c_write_blocking(i2c_default, MPU_ADDRESS, buf, 2, false);
-}
+QueueHandle_t xQueueData;
 
-static void mpu6050_read_raw(int16_t accel[3], int16_t gyro[3], int16_t *temp) {
-    // For this particular device, we send the device the register we want to read
-    // first, then subsequently read from the device. The register is auto incrementing
-    // so we don't need to keep sending the register we want, just the first.
+typedef struct {
+    int axis;
+    int value;
+} imu_data_t;
 
-    uint8_t buffer[6];
-
-    // Start reading acceleration registers from register 0x3B for 6 bytes
-    uint8_t val = 0x3B;
-    i2c_write_blocking(i2c_default, MPU_ADDRESS, &val, 1, true); // true to keep master control of bus
-    i2c_read_blocking(i2c_default, MPU_ADDRESS, buffer, 6, false);
-
-    for (int i = 0; i < 3; i++) {
-        accel[i] = (buffer[i * 2] << 8 | buffer[(i * 2) + 1]);
-    }
-
-    // Now gyro data from reg 0x43 for 6 bytes
-    // The register is auto incrementing on each read
-    val = 0x43;
-    i2c_write_blocking(i2c_default, MPU_ADDRESS, &val, 1, true);
-    i2c_read_blocking(i2c_default, MPU_ADDRESS, buffer, 6, false);  // False - finished with bus
-
-    for (int i = 0; i < 3; i++) {
-        gyro[i] = (buffer[i * 2] << 8 | buffer[(i * 2) + 1]);;
-    }
-
-    // Now temperature from reg 0x41 for 2 bytes
-    // The register is auto incrementing on each read
-    val = 0x41;
-    i2c_write_blocking(i2c_default, MPU_ADDRESS, &val, 1, true);
-    i2c_read_blocking(i2c_default, MPU_ADDRESS, buffer, 2, false);  // False - finished with bus
-
-    *temp = buffer[0] << 8 | buffer[1];
-}
-
-void mpu6050_task(void *p) {
+static void mpu6050_init() {
     i2c_init(i2c_default, 400 * 1000);
     gpio_set_function(I2C_SDA_GPIO, GPIO_FUNC_I2C);
     gpio_set_function(I2C_SCL_GPIO, GPIO_FUNC_I2C);
     gpio_pull_up(I2C_SDA_GPIO);
     gpio_pull_up(I2C_SCL_GPIO);
 
-    mpu6050_reset();
-    int16_t acceleration[3], gyro[3], temp;
+    // Reseta o MPU6050
+    uint8_t buf[] = {0x6B, 0x00}; // Reset
+    int result = i2c_write_blocking(i2c_default, MPU_ADDRESS, buf, 2, false);
+    if (result == PICO_ERROR_GENERIC) {
+        printf("Erro ao inicializar MPU6050!\n");
+    }
+}
 
-    while(1) {
-        mpu6050_read_raw(acceleration, gyro, &temp);
-        printf("Acc. X = %d, Y = %d, Z = %d\n", acceleration[0], acceleration[1], acceleration[2]);
-        printf("Gyro. X = %d, Y = %d, Z = %d\n", gyro[0], gyro[1], gyro[2]);
-        printf("Temp. = %f\n", (temp / 340.0) + 36.53);
+static void mpu6050_read_raw(int16_t accel[3], int16_t gyro[3], int16_t *temp) {
+    uint8_t buffer[6];
+    uint8_t val = 0x3B;
 
-        vTaskDelay(pdMS_TO_TICKS(10));
+    // Lê os dados de aceleração
+    if (i2c_write_blocking(i2c_default, MPU_ADDRESS, &val, 1, true) == PICO_ERROR_GENERIC) {
+        printf("Erro ao escrever no MPU6050\n");
+        return;
+    }
+    i2c_read_blocking(i2c_default, MPU_ADDRESS, buffer, 6, false);
+    for (int i = 0; i < 3; i++) {
+        accel[i] = (buffer[i * 2] << 8 | buffer[(i * 2) + 1]);
     }
 
+    // Lê os dados do giroscópio
+    val = 0x43;
+    i2c_write_blocking(i2c_default, MPU_ADDRESS, &val, 1, true);
+    i2c_read_blocking(i2c_default, MPU_ADDRESS, buffer, 6, false);
+    for (int i = 0; i < 3; i++) {
+        gyro[i] = (buffer[i * 2] << 8 | buffer[(i * 2) + 1]);
+    }
 
+    // Lê a temperatura
+    val = 0x41;
+    i2c_write_blocking(i2c_default, MPU_ADDRESS, &val, 1, true);
+    i2c_read_blocking(i2c_default, MPU_ADDRESS, buffer, 2, false);
+    *temp = buffer[0] << 8 | buffer[1];
+}
+
+void data_task(void *p) {
+    imu_data_t data;
+
+    while (1) {
+        if (xQueueReceive(xQueueData, &data, portMAX_DELAY) == pdPASS) {
+            // Enviar os dados pela UART
+            uart_putc(UART0_BASE, data.axis);
+            uart_putc(UART0_BASE, (data.value >> 8) & 0xFF);
+            uart_putc(UART0_BASE, data.value & 0xFF);
+            uart_putc(UART0_BASE, 0xFF); // Byte de fim de pacote
+        }
+    }
+}
+
+void mpu6050_task(void *p) {
+    mpu6050_init();
+
+    int16_t acceleration[3], gyro[3], temp;
+    FusionAhrs ahrs;
+    FusionAhrsInitialise(&ahrs);
+
+    while (1) {
+        // Lê os dados da IMU
+        mpu6050_read_raw(acceleration, gyro, &temp);
+
+
+        FusionVector gyroscope = {
+            .axis.x = gyro[0] / 131.0f,
+            .axis.y = gyro[1] / 131.0f,
+            .axis.z = gyro[2] / 131.0f,
+        };
+        FusionVector accelerometer = {
+            .axis.x = acceleration[0] / 16384.0f,
+            .axis.y = acceleration[1] / 16384.0f,
+            .axis.z = acceleration[2] / 16384.0f,
+        };
+
+        FusionAhrsUpdateNoMagnetometer(&ahrs, gyroscope, accelerometer, SAMPLE_PERIOD);
+        FusionEuler euler = FusionQuaternionToEuler(FusionAhrsGetQuaternion(&ahrs));
+
+        imu_data_t data;
+        data.axis = 0;
+        data.value = (int)euler.angle.yaw;
+        xQueueSend(xQueueData, &data, portMAX_DELAY);
+
+        data.axis = 1;
+        data.value = (int)euler.angle.roll;
+        xQueueSend(xQueueData, &data, portMAX_DELAY);
+
+
+        if (accelerometer.axis.y > 1.5f) {
+                data.axis = 2;
+            data.value = 1;
+            xQueueSend(xQueueData, &data, portMAX_DELAY);
+        }
+
+        vTaskDelay(pdMS_TO_TICKS(10));  // Atraso para a próxima leitura
+    }
 }
 
 int main() {
     stdio_init_all();
-
-    xTaskCreate(mpu6050_task, "mpu6050_Task 1", 8192, NULL, 1, NULL);
-
+    xQueueData = xQueueCreate(10, sizeof(imu_data_t));  // Fila para armazenar os dados do IMU
+    xTaskCreate(mpu6050_task, "mpu6050_task", 8192, NULL, 1, NULL);
+    xTaskCreate(data_task, "data_task", 1024, NULL, 1, NULL);
     vTaskStartScheduler();
-
-    while (true)
-        ;
+    while (true);
 }
---
+++ python/main.py
@@ -0,0 +1,57 @@
+import serial
+import uinput
+import time
+
+ser = serial.Serial('/dev/ttyACM0', 115200)
+
+# Create new mouse device
+device = uinput.Device([
+    uinput.BTN_LEFT,
+    uinput.BTN_RIGHT,
+    uinput.REL_X,
+    uinput.REL_Y,
+])
+
+
+def parse_data(data):
+    axis = data[0]  # 0 for X, 1 for Y
+    value = int.from_bytes(data[1:3], byteorder='little', signed=True)
+    print(f"Received data: {data}")
+    print(f"axis: {axis}, value: {value}")
+    return axis, value
+
+
+def move_mouse(axis, value):
+    if axis == 0:    # X-axis
+        device.emit(uinput.REL_X, value)
+    elif axis == 1:  # Y-axis
+        device.emit(uinput.REL_Y, value)
+    elif axis == 2:
+        if value == 1:
+            device.emit(uinput.BTN_LEFT, 1)
+            time.sleep(0.1)
+            device.emit(uinput.BTN_LEFT, 0)
+
+
+try:
+    # sync package
+    while True:
+        print('Waiting for sync package...')
+        while True:
+            data = ser.read(1)
+            if data == b'\xff':
+                break
+            else:
+                print(f"Received error: {data}")
+
+        # Read 4 bytes from UART
+        data = ser.read(3)
+        axis, value = parse_data(data)
+        move_mouse(axis, value)
+
+except KeyboardInterrupt:
+    print("Program terminated by user")
+except Exception as e:
+    print(f"An error occurred: {e}")
+finally:
+    ser.close()
